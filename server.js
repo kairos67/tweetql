a3b216069e2b7750ba175101bc594ce0f8f78e5f
kairos67 <kairosys@gmail.com>
2022-09-29 20:22:09 +0900

    query resolvers

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // graphql, apollo server test
 import { ApolloServer, gql } from "apollo-server";
+const tweets =[
+    {
+        id: "1",
+        text: "Hello, world 1!",
+    },
+    {
+        id: "2",
+        text: "Hello, world 2!",
+    },
+];
 
 const typeDefs = gql`
     type User{
@@ -16,6 +26,7 @@ const typeDefs = gql`
     type Query{
         allTweets: [Tweet]
         tweet(id:ID!): Tweet
+        ping: String
     }    
     type Mutation{
         postTweet(text:String!, userId:ID!): Tweet!
@@ -26,7 +37,27 @@ const typeDefs = gql`
 //GET /api/v1/tweeters
 //post /api/v1/tweets
 //GET /api/v1/tweet/:id
-const server = new ApolloServer({ typeDefs });
+
+const resolvers ={
+    Query:{
+        allTweets(){
+            return tweets;
+        },
+        // tweet() {
+        //      console.log("I'm a tweet");
+        //     return null;
+        // },
+        tweet(root, {id}) {
+            // console.log(args);
+            return tweets.find((tweet)=> tweet.id === id);
+        },
+        // ping(){
+        //     return "pong";
+        // },
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
